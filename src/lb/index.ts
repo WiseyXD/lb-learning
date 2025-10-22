ad641add298ecd80539c78d34038b430d41a0519
WiseyXD <aryan.s.nag@gmail.com>
2025-10-22 23:37:56 +0200

    build : lb refactored

--- src/lb/index.ts
+++ src/lb/index.ts
@@ -8,6 +8,16 @@ let maxCounterFor3 = 3;
 let maxCounterFor2 = 2;
 let maxCounterFor1 = 1;
 
+const servers = [
+    { port: 3001, weight: 1 },
+    { port: 3002, weight: 2 },
+    { port: 3003, weight: 3 },
+];
+
+let weightedList = servers.flatMap(s => Array(s.weight).fill(s.port));
+let currentIndex = 0;
+
+
 const randomIndex = () => {
     const numberOfServers = ports.length;
     const random = Math.random();
@@ -30,60 +40,74 @@ app.get('/random', async (c) => {
 })
 
 app.get('/round-robin', async (c) => {
-    if (counter == 0) {
-        const response = await fetch('http://localhost:3001');
-        const data = await response.text();
-        console.log(data)
-        counter++;
-        return c.text(data);
-    }
-    else if (counter == 1) {
+    // if (counter == 0) {
+    //     const response = await fetch('http://localhost:3001');
+    //     const data = await response.text();
+    //     console.log(data)
+    //     counter++;
+    //     return c.text(data);
+    // }
+    // else if (counter == 1) {
 
-        const response = await fetch('http://localhost:3002');
-        const data = await response.text();
-        console.log(data)
-        counter++;
-        return c.text(data);
-    }
-    else {
-        const response = await fetch('http://localhost:3003');
-        const data = await response.text();
-        console.log(data)
-        counter = 0;
-        return c.text(data);
-    }
+    //     const response = await fetch('http://localhost:3002');
+    //     const data = await response.text();
+    //     console.log(data)
+    //     counter++;
+    //     return c.text(data);
+    // }
+    // else {
+    //     const response = await fetch('http://localhost:3003');
+    //     const data = await response.text();
+    //     console.log(data)
+    //     counter = 0;
+    //     return c.text(data);
+    // }
+
+    const selectedPort = ports[counter % ports.length];
+    counter++;
+    const response = await fetch(`http://localhost:${selectedPort}`);
+    const data = await response.text();
+    return c.text(data);
+
 })
 
 app.get('/weighted', async (c) => {
     // lets say server 1 can sserver 3 , server2 => 2 ,server1 => 1
-    if (maxCounterFor3 < 1) {
-        if (maxCounterFor2 < 1) {
-            if (maxCounterFor1 < 1) {
-                maxCounterFor1 = 1;
-                maxCounterFor2 = 2;
-                maxCounterFor3 = 3;
-                return c.text("All servers are occupied !");
+    // if (maxCounterFor3 < 1) {
+    //     if (maxCounterFor2 < 1) {
+    //         if (maxCounterFor1 < 1) {
+    //             maxCounterFor1 = 1;
+    //             maxCounterFor2 = 2;
+    //             maxCounterFor3 = 3;
+    //             return c.text("All servers are occupied !");
 
-            }
-            else {
-                const response = await fetch('http://localhost:3001');
-                const data = await response.text();
-                maxCounterFor1--;
-                return c.text(data);
-            }
-        }
-        else {
-            const response = await fetch('http://localhost:3002');
-            const data = await response.text();
-            maxCounterFor2--;
-            return c.text(data);
-        }
-    } else {
-        const response = await fetch('http://localhost:3003');
-        const data = await response.text();
-        maxCounterFor3--;
-        return c.text(data);
-    }
+    //         }
+    //         else {
+    //             const response = await fetch('http://localhost:3001');
+    //             const data = await response.text();
+    //             maxCounterFor1--;
+    //             return c.text(data);
+    //         }
+    //     }
+    //     else {
+    //         const response = await fetch('http://localhost:3002');
+    //         const data = await response.text();
+    //         maxCounterFor2--;
+    //         return c.text(data);
+    //     }
+    // } else {
+    //     const response = await fetch('http://localhost:3003');
+    //     const data = await response.text();
+    //     maxCounterFor3--;
+    //     return c.text(data);
+    // }
+
+    const selectedPort = weightedList[currentIndex];
+    currentIndex = (currentIndex + 1) % weightedList.length;
+    const response = await fetch(`http://localhost:${selectedPort}`);
+    const data = await response.text();
+    return c.text(data);
+
 })
 
 export default {
